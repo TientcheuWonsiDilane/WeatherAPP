f8658f42247515ad2394f4e49c6493846adc09c8
TientcheuWonsiDilane <dilanetientcheu67@gmail.com>
2026-07-20 17:55:53 +0100

    gsap

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -9,14 +9,43 @@ import {
   Wind,
 } from "lucide-react";
 import axios from "axios";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const App = () => {
-  const [image, setImage] = useState(
-    "https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bight-sun-and-clouds.jpg?s=612x612&w=0&k=20&c=MGd2-v42lNF7Ie6TtsYoKnohdCfOPFSPQt5XOz4uOy4=" || "./assets/hero.png"
+
+   const [image, setImage] = useState(
+    "https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bight-sun-and-clouds.jpg?s=612x612&w=0&k=20&c=MGd2-v42lNF7Ie6TtsYoKnohdCfOPFSPQt5XOz4uOy4=" ||
+      "./assets/hero.png",
   );
   const [loading, setLoading] = useState(false);
   const [weather, setWeather] = useState({});
   const [city, setCity] = useState("Yaounde");
+
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      delay: 1,
+      defaults: {
+        ease: "power2.out",
+        duration: 1.7,
+      },
+    });
+
+    tl.from("#metrics", {
+      opacity: 0,
+      y: 500,
+    }).from(
+      ".text",
+      {
+        opacity: 0,
+        y: 50,
+        stagger: 0.5,
+      },
+      "-=0.2",
+    );
+  }, {dependencies: [city]});
+
+ 
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -84,7 +113,6 @@ const App = () => {
       className="min-h-screen w-full bg-cover bg-center text-white overflow-hidden"
       style={{
         backgroundImage: `url(${image})`,
-        opacity: 0.7,
       }}>
       <header className="py-3 flex flex-col md:flex-row gap-3 justify-center items-center md:justify-evenly">
         <h1 className="text-3xl tracking-tighter font-black text-shadow-lg">
@@ -93,9 +121,11 @@ const App = () => {
         <form
           onSubmit={handleSubmit}
           className=" flex flex-col items-center justify-center md:flex-row gap-3 md:gap-6">
-          <label for="city" className="font-black text-lg text-shadow-lg">
+          <label
+            for="city"
+            className="font-black text-lg text-shadow-lg flex items-center justify-center">
             {" "}
-            City :
+            <span>City : </span>
             <input
               type="text"
               placeholder="Enter a city"
@@ -106,9 +136,9 @@ const App = () => {
           </label>
           <button
             type="submit"
-            className=" flex gap-2 px-4 py-2 bg-sky-600 text-white rounded-full">
+            className=" flex  items-center justify-center gap-2 px-4 py-2 bg-sky-600 text-white rounded-full">
             <CloudLightning size={20} />
-            <span className="text-lg">get Weather</span>
+            <span className="text-sm lg:text-lg">get Weather</span>
           </button>
         </form>
         <nav>
@@ -125,24 +155,31 @@ const App = () => {
           </ul>
         </nav>
       </header>
-      <main className="p-4 md:p-12 font-black flex flex-col lg:grid lg:grid-cols-2">
-        <div className="h-[35vh] lg:h-screen">
+      <main className="p-4  gap-12 md:p-12 font-black flex flex-col lg:grid lg:grid-cols-2">
+        <div id="current" className="h-[35vh] lg:h-screen  ">
           <div className=" absolute top-[45vh] lg:top-1/2 -translate-y-1/2 p-12 ">
-            <div className=" text-7xl lg:text-9xl">{weather?.main?.temp}°C</div>
-            <h1 className="text-3xl lg:text-7xl text-shadow-lg flex gap-2 items-center ">
-              <MapPin size={60} />
-              {weather.name} {weather.sys?.country}
+            <div className=" text text-7xl text-sky-600 lg:text-9xl">
+              {weather ? weather?.main?.temp : "0.00" }°C
+            </div>
+            <h1 className=" text text-3xl lg:text-7xl text-shadow-lg flex gap-2 items-center ">
+              <MapPin className=" aspect-2/3" width={60} height={80} />
+              <div className="flex flex-col">
+                {weather.name} {weather ? weather.sys?.country : "CM"}
+                <span className="p-2 text-xl lg:text-2xl">
+                  {weather ? weather?.weather?.[0]?.description : "Sunny"}{" "}
+                </span>
+              </div>
             </h1>
-            <p className="p-5 text-xl lg:text-2xl">
-              {weather?.weather?.[0]?.description}{" "}
-              <p className="text-xl lg:text-2xl font-bold lg:ml-6">
-                {weather.timezone ? getLocalTime(weather.timezone) : " --:--"}
-              </p>
+
+            <p className=" text text-xl  lg:text-2xl font-black lg:ml-6">
+              {weather.timezone ? getLocalTime(weather.timezone) : "01 Jan, --:--"}
             </p>
           </div>
         </div>
-        <div className=" border border-white rounded-3xl p-4 md:p-12 backdrop-blur-md h-[70vh] lg:h-screen overflow-hidden">
-          <h2 className="text-3xl font-bold tracking-tighter text-center pb-4">
+        <div
+          id="metrics"
+          className=" border border-white rounded-3xl p-4 md:p-12 backdrop-blur-md h-[70vh] lg:h-screen overflow-hidden">
+          <h2 className="text-3xl text-sky-600 font-bold tracking-tighter text-center pb-4">
             Current Metrics
           </h2>
           <div className="w-[80%] mx-auto flex flex-col  gap-16 lg:gap-20">
